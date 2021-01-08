8369a495d59ba9004a7e3592e7578514ac4692eb
Antoni Pawlak <antoni.pawlak@protonmail.com>
2021-01-08 23:19:33 +0100

    Fixed bug with firstapp

---
+++ 1-firstapp/Main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go!")
+}
+
+// go run src/github.com/antooni/firstapp
+// go build github.com/antooni/firstapp
+// go install github.com/antooni/firstapp
+
+// https://golang.org/doc/effective_go.html
